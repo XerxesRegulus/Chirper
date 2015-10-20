fa4aadabc4e4f50d244d812a94ce0e938f6fdcb8
John Paul Ricarse <johnpauliloilo@gmail.com>
2015-10-20 17:32:27 +0800

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007072230) do
+ActiveRecord::Schema.define(version: 20151020084901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20151007072230) do
     t.datetime "updated_at",        null: false
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -61,6 +68,7 @@ ActiveRecord::Schema.define(version: 20151007072230) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "follower_id"
     t.string   "authentication_token"
   end
 
